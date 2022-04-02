6e1b60e5a53210ba5d4ff7ce4b789d7ecda04f46
James Harkins <jh.harkins@protonmail.com>
2022-04-01 19:12:47 -0600

    test: create model test for project average_cost_per_item instance method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Project, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'instance methods' do
+    it '#average_cost_per_item' do
+      project = Project.create!(name: "NAMM 2023", manager: "James Harkins")
+
+      fender = Manufacturer.create!(name: "Fender", location: "Los Angeles")
+      another_new_strat = fender.items.create!(name: "The Newest Strat", cost: 1507, project_id: project.id)
+      another_new_tele = fender.items.create!(name: "The Newest Tele", cost: 1302, project_id: project.id)
+
+      gibson = Manufacturer.create!(name: "Gibson", location: "Nashville")
+      another_new_paul = gibson.items.create!(name: "The Newest Les Paul", cost: 2501, project_id: project.id)
+      another_new_sg = gibson.items.create!(name: "The Newest SG", cost: 2009, project_id: project.id)
+      another_new_v = gibson.items.create!(name: "The Newest Flying V", cost: 2502, project_id: project.id)
+
+      expect(project.average_cost_per_item).to eq(1964.20)
+    end
+  end
 end
