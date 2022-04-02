45b3e65993675012187c34d382261ab9ad626828
James Harkins <jh.harkins@protonmail.com>
2022-04-01 18:46:53 -0600

    test: create feature tests for items#show page manufacturer_count for user story 3

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe 'item show page' do
       before :each do
         @project = Project.create!(name: "NAMM 2023", manager: "James Harkins")
         @another_new_strat = @project.items.create!(name: "The Newest Strat", cost: 1500)
-        @another_new_tele = @project.items.create!(name: "The Newest Tele", cost: 1300)
+        @another_new_paul = @project.items.create!(name: "The Newest Paul", cost: 2500)
+        @fender = @another_new_strat.manufacturers.create!(name: "Fender Guitars", location: "Los Angeles")
+        @ernie_ball = @another_new_strat.manufacturers.create!(name: "Ernie Ball Strings", location: "San Luis Obispo")
+        @gibson = @another_new_paul.manufacturers.create!(name: "Gibson", location: "Nashville")
 
         visit "/items/#{@another_new_strat.id}"
       end
@@ -14,12 +17,16 @@ RSpec.describe 'item show page' do
       it 'i see that items name and cost' do
         expect(page).to have_content("#{@another_new_strat.name}")
         expect(page).to have_content("Cost: #{@another_new_strat.cost}")
-        expect(page).not_to have_content("#{@another_new_tele.name}")
-        expect(page).not_to have_content("Cost: #{@another_new_tele.cost}")
+        expect(page).not_to have_content("#{@another_new_paul.name}")
+        expect(page).not_to have_content("Cost: #{@another_new_paul.cost}")
       end
 
-      it 'i also see the name of the project the item belongs to' do
+      it 'i see the name of the project the item belongs to' do
         expect(page).to have_content("Project: #{@project.name}")
+      end
+
+      it 'i see a count of the number of manufacturers for the item' do
+        expect(page).to have_content("Manufacturer Count: 2")
       end
     end
   end
