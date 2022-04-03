192f09e8a7b9aeac375b5c7ce21510a66f76b20f
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-03 14:29:15 -0600

    Test: Projects model calculates average cost of items to two decimal places

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Project, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  it "calculates average cost per item to two decimal places" do
+    project = Project.create!(name: "Eat Road Runner", manager: "Wile E. Coyote")
+    item_1 = project.items.create!(name: "Anvil", cost: 200)
+    item_2 = project.items.create!(name: "Trampoline", cost: 899.49)
+
+    expect(project.average_item_cost).to eq(549.75)
+  end
 end
