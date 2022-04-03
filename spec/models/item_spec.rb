90eeb2c0317f88b7875a6f53e96391733b8cbcc5
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-03 14:17:14 -0600

    Test: Item model finds number of manufacturers associated with item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Item, type: :model do
     it { should have_many :manufacturer_items }
     it { should have_many(:manufacturers).through(:manufacturer_items) }
   end
+
+  it "counts manufacturers associated with item" do
+    project = Project.create!(name: "Eat Road Runner", manager: "Wile E. Coyote")
+    item_1 = project.items.create!(name: "Anvil", cost: 200)
+    item_2 = project.items.create!(name: "Trampoline", cost: 800)
+
+    acme = Manufacturer.create!(name: "ACME", location: "Toonville USA")
+    offbrand = Manufacturer.create!(name: "Offbrand ACME", location: "Worseville USA")
+
+    item_1.manufacturers << acme
+    item_2.manufacturers << acme
+    item_2.manufacturers << offbrand
+
+    expect(item_1.manufacturer_count).to eq(1)
+    expect(item_2.manufacturer_count).to eq(2)
+  end
 end
