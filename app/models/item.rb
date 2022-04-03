229d07842f93120c01212591af5ab048ec7b075d
Andrew Speth <speth.andrew@yahoo.com>
2022-04-03 09:56:31 -0600

    model test passes for #manufacturer_count

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   belongs_to :project
   has_many :manufacturer_items
   has_many :manufacturers, through: :manufacturer_items
+
+  def manufacturer_count
+    manufacturers.count
+  end
 end
