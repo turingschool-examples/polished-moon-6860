15d37297da3d3925b781b6c9526a951a755872e0
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-03 12:24:35 -0600

    Updates prelim item show page - user story 1 complete

--- spec/features/item/show_spec.rb
+++ spec/features/item/show_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "Item show page", type: :feature do
-  let!(:e_stuff) { Project.create!(name: "e_stuff", manager: "Jim Cook") }
-  let!(:ephone) { Item.create!(name: "ePhone", cost: 900, project_id: e_stuff.id) }
-  # let!(:epad) { Item.create!(name: "ePad", cost: 500, project_id: e_stuff.id) }
+  let!(:estuff) { Project.create!(name: "eStuff", manager: "Jim Cook") }
+  let!(:ephone) { Item.create!(name: "ePhone", cost: 900, project_id: estuff.id) }
 
   it "lists item name/cost/project" do
     visit "/items/#{ephone.id}"
 
-    expect(page).to have_content(ephone.name)
-    expect(page).to have_content(ephone.cost)
-    # expect(page).to have_content(ephone.#project name)
+    expect(page).to have_content("ePhone")
+    expect(page).to have_content("900")
+    expect(page).to have_content("eStuff")
   end
 end
