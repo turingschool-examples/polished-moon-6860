8218624cb54376b158f54ee50ba587976a3be099
Chris Romero <chrisleeromero1@gmail.com>
2022-04-03 14:17:57 -0600

    refactor: update item show page and item controller to use new item count method

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,6 +2,6 @@ class ItemController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @project = @item.project
-    @manufacturers = @item.manufacturers
+    @manufacturers = @item.manufacturer_count
   end
 end
