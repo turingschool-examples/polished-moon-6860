4848f4231dc53cfd8be32c5720731728bb6d8bdf
Andrew Speth <speth.andrew@yahoo.com>
2022-04-03 10:22:18 -0600

    feature test passes for #average_item_price

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "item show page" do
-  xit "displays the item's name, cost, and associated projects" do
+  it "displays the item's name, cost, and associated projects" do
     fence = Project.create!(name: "fence repair", manager: "Bob the Builder")
     hammer = Item.create!(name: "hammer", cost: 3, project_id: fence.id)
     drill = Item.create!(name: "drill", cost: 5, project_id: fence.id)
