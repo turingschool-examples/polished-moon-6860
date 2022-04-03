a19dc1ccdfdefb5bf729cce580f5978b8b46e6a5
Andrew Speth <speth.andrew@yahoo.com>
2022-04-03 10:21:43 -0600

    model test passes for #average_item_price

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/items/:id', to: 'items#show'
   
+  get '/projects/:id', to: 'projects#show'
+
   get '/manufacturers', to: 'manufacturers#index'
 end
