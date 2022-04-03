a6af9515f3ce7093427a2feb81ad9571cfa73995
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-03 14:39:59 -0600

    Model: Projects model calculates average cost of items to two decimal places

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ApplicationRecord
   validates_presence_of :name, :manager
   has_many :items
+
+  def average_item_cost
+    costs_array = items.map { |item| item.cost  }
+    (costs_array.sum(0.00) / costs_array.count).round(2)
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -13,8 +13,9 @@ RSpec.describe Project, type: :model do
   it "calculates average cost per item to two decimal places" do
     project = Project.create!(name: "Eat Road Runner", manager: "Wile E. Coyote")
     item_1 = project.items.create!(name: "Anvil", cost: 200)
-    item_2 = project.items.create!(name: "Trampoline", cost: 899.49)
+    item_2 = project.items.create!(name: "Trampoline", cost: 903)
+    item_3 = project.items.create!(name: "Firecrackers", cost: 500)
 
-    expect(project.average_item_cost).to eq(549.75)
+    expect(project.average_item_cost).to eq(534.33)
   end
 end
