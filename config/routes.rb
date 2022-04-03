357bd64be859d09968ed711d29da70ba12b29a24
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-03 11:27:20 -0600

    ModelTest: project#average_price should return average item price

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/projects/:id', to: 'projects#show'
+
   get '/items/:id', to: 'items#show'
 
   get '/manufacturers', to: 'manufacturers#index'
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe Project, type: :model do
   describe 'relationships' do
     it { should have_many :items }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @project = Project.create!(name: "Create Life", manager: "Dr. Frankenstein")
+
+      @lab = Manufacturer.create!(name: "Frankenstein's Lab", location: "Ingolstadt, Germany")
+      @table = @lab.items.create!(name: "Steel Lab Table", cost: 523.64, project_id: @project.id)
+      @roof = @lab.items.create!(name: "Retractable Roof", cost: 10054.55, project_id: @project.id)
+
+      @graveyard = Manufacturer.create!(name: "Ingolstadt Graveyard", location: "Ingolstadt, Germany")
+      @limbs = @graveyard.items.create!(name: "Some Limbs", cost: 18.46, project_id: @project.id)
+      @head = @graveyard.items.create!(name: "A head", cost: 17.33, project_id: @project.id)
+      @torso = @graveyard.items.create!(name: "A torso", cost: 23.65, project_id: @project.id)
+    end
+
+    it '#average_price should return the average price per item' do
+      expect(@project.average_price).to eq(2127.53)
+    end
+  end
 end
