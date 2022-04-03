8a297dd0285caa193a28d0d55bd34f460149e561
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-03 13:51:45 -0600

    Adds projects show page - user story extension 1 (mostly) complete Adds #average_item_cost method + test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project, type: :model do
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :manager }
   end
 
-  describe 'relationships' do
+  describe "relationships" do
     it { should have_many :items }
+  end
+
+  let!(:estuff) { Project.create!(name: "eStuff", manager: "Jim Cook") }
+  let!(:ephone) { Item.create!(name: "ePhone", cost: 900, project_id: estuff.id) }
+  let!(:epad) { Item.create!(name: "ePad", cost: 400, project_id: estuff.id) }
+
+  it "averages cost of all associated items" do
+    expect(estuff.average_item_cost).to eq(650)
   end
 end
