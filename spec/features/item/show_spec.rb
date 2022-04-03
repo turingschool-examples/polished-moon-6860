781d55b85a9345c80c4c1923b500b90f5e612794
Phillip Kamps <phillip.kamps@gmail.com>
2022-04-03 13:14:10 -0600

    Updates item show spec

--- spec/features/item/show_spec.rb
+++ spec/features/item/show_spec.rb
@@ -3,6 +3,10 @@ require "rails_helper"
 RSpec.describe "Item show page", type: :feature do
   let!(:estuff) { Project.create!(name: "eStuff", manager: "Jim Cook") }
   let!(:ephone) { Item.create!(name: "ePhone", cost: 900, project_id: estuff.id) }
+  let!(:fexcamm) { Manufacturer.create!(name: "FexCamm", location: "Wisconsin") }
+  let!(:tegatron) { Manufacturer.create!(name: "Tegatron", location: "Taiwan") }
+  let!(:man_item_1) { ManufacturerItem.create!(item_id: ephone.id, manufacturer_id: fexcamm.id) }
+  let!(:man_item_2) { ManufacturerItem.create!(item_id: ephone.id, manufacturer_id: tegatron.id) }
 
   it "lists item name/cost/project" do
     visit "/items/#{ephone.id}"
@@ -10,5 +14,6 @@ RSpec.describe "Item show page", type: :feature do
     expect(page).to have_content("ePhone")
     expect(page).to have_content("900")
     expect(page).to have_content("eStuff")
+    expect(page).to have_content("2")
   end
 end
