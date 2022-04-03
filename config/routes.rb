028bb08e9048b7e5d73bd3b54c79cc4147e061f9
John Hennerich <john.hennerich@gmail.com>
2022-04-03 15:53:17 -0600

    Feat: create route,controller entries for manufacturers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/items/:id', to: 'items#show'
+  get '/manufacturers', to: 'manufacturers#index'
 end
--- spec/features/manufacturers/index_spec.rb
+++ spec/features/manufacturers/index_spec.rb
@@ -6,10 +6,22 @@ require 'rails_helper'
 
 RSpec.describe 'when I visit the manufacturers index page' do
   before do
+    @hammer = Manufacturer.create!(name:"Hammers R us", location:"123 Anywhere USA")
+    @drill = Manufacturer.create!(name:"Drills R us", location:"123 AnywhereElse USA")
 
+    @project_1 = Project.create!(name:"John House", manager: "Bob")
+
+    @item_1 = @project_1.items.create!(name:"Hammer", cost: 20)
+    @item_2 = @project_1.items.create!(name:"Drill", cost:100)
+
+    visit('/manufacturers')
   end
   describe 'display' do
     it 'list all names of manufacturers' do
+
+      expect(current_path).to eq('/manufacturers')
+      expect(page).to have_content(@hammer.name)
+      expect(page).to have_content(@drill.name)
 
     end
     it 'each manufacturer name list all names of items' do
