0fd3046fc35d40e4e43b4443a740a7141aaf7d29
Chris Romero <chrisleeromero1@gmail.com>
2022-04-03 13:04:14 -0600

    refactor: create project variable in item controller

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,5 +2,6 @@ class ItemController < ApplicationController
   def show
     # require 'pry'; binding.pry
     @item = Item.find(params[:id])
+    @project = @item.project
   end
 end
--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,10 +1,4 @@
 require 'rails_helper'
-# User Story 1 of 3
-#
-# As a visitor,
-# When I visit an item's show page ("/items/:id"),
-# I see that item's name and cost
-# And I also see the name of the project this belongs to.
 
 RSpec.describe 'item show page', type: :feature do
   it 'displays the items name and cost and project it belongs to' do
