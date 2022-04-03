c9438e405a3ff579b1f75edb4ad2c4b8e80be6e9
John Hennerich <john.hennerich@gmail.com>
2022-04-03 15:21:06 -0600

    Test: add project and items to spec file

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -7,13 +7,22 @@ require 'rails_helper'
 RSpec.describe 'when i visit an items show page'do
 
   before do
-    @item_1 =
+
+    @project_1 = Project.create!(name:"John House", manager: "Bob")
+
+    @item_1 = @project_1.items.create!(name:"Hammer", cost: 20)
+    @item_2 = @project_1.items.create!(name:"Drill", cost:100)
+
 
   visit "/items/#{@item_1.id}"
   end
 
   describe 'display' do
     it 'item name, cost and project item belongs to' do
+
+      expect(page).to have_content(@item_1.name)
+      expect(page).to have_content(@item_1.cost)
+      expect(page).to have_content(@project_1.name)
 
     end
   end
