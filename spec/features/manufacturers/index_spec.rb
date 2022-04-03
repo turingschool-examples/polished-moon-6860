2de4f07912cb03db982203ef9a1cba3aebec8d79
John Hennerich <john.hennerich@gmail.com>
2022-04-03 15:39:18 -0600

    Test: build skell of manufactures spec

---
+++ spec/features/manufacturers/index_spec.rb
@@ -0,0 +1,19 @@
+# As a visitor,
+# When I visit the manufacturers index page ("/manufacturers")
+# I see a list of names of all the manufacturers
+# And under each manufacturers  name I see a list of all of the names of the items they belong to.
+require 'rails_helper'
+
+RSpec.describe 'when I visit the manufacturers index page' do
+  before do
+
+  end
+  describe 'display' do
+    it 'list all names of manufacturers' do
+
+    end
+    it 'each manufacturer name list all names of items' do
+
+    end
+  end
+end
