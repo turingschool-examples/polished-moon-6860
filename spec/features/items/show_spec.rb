1adde135a9b9544b4fcbd130128c48d09f608437
Andrew Speth <speth.andrew@yahoo.com>
2022-04-03 10:34:49 -0600

    test: items#show has a form to add a manufacturer

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -31,8 +31,37 @@ RSpec.describe "item show page" do
     
     expect(page).to have_content("#{hammer.manufacturer_count}")
   end
+  
+  it "has a form to add a manufacturer" do
+    fence = Project.create!(name: "fence repair", manager: "Bob the Builder")
+    hammer = Item.create!(name: "hammer", cost: 3, project_id: fence.id)
+    drill = Item.create!(name: "drill", cost: 5, project_id: fence.id)
+    boards = Item.create!(name: "boards", cost: 4, project_id: fence.id)
+    
+    ryobi = Manufacturer.create!(name: "Ryobi", location: "Denver")
+    board_maker = Manufacturer.create!(name: "Board Maker", location: "The Forest")
+    hammer_co = Manufacturer.create!(name: "Hammer Company", location: "Hamburg")
+    
+    manufacturer_item1 = ManufacturerItem.create!(manufacturer_id: ryobi.id, item_id: hammer.id)
+    manufacturer_item2 = ManufacturerItem.create!(manufacturer_id: ryobi.id, item_id: drill.id)
+    manufacturer_item3 = ManufacturerItem.create!(manufacturer_id: board_maker.id, item_id: boards.id)
+    
+    visit "/items/#{hammer.id}"
+    
+    expect(page).to have_content("Manufacturer Count: 1")
+    
+    fill_in 'Manufacturer id', with: "#{hammer_co.id}"
+    click_on "Add"
+    
+    expect(page).to have_content("Manufacturer Count: 2")
+  end
 end
-
 # As a visitor,
-# When I visit an item's show page
-# I see a count of the number of manufacturers for this item
+# When I visit a item's show page
+# I see a form to add a manufacturer to this project
+# When I fill out a field with an existing manufacturer's id
+# And hit "Add manufacturer to item"
+# I'm taken back to the item's show page
+# And I see that the number of manufacturers has increased by 1
+# And when I visit the manufacturers index page
+# I see that item listed under that manufacturer's name
