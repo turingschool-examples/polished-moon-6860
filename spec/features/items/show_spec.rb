431af102f2483ebe56f854e5c3f49d0c4fa3e426
reuben <creubend@gmail.com>
2022-04-04 17:53:02 -0600

    test: Item show page test created

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,13 @@
+# As a visitor,
+# When I visit an item's show page ("/items/:id"),
+# I see that item's name and cost
+# And I also see the name of the project this belongs to.
+require "rails_helper"
+RSpec.describe 'Item show page' do
+  it "contains item name and costs" do
+    project = Project.create!(name: 'newbuild', manager: 'Rick')
+    item = project.items.create!(name: 'thingamabob', cost: 12)
+    visit "/items/#{item.id}"
+    
+  end
+end
