bdd3b09bbb5cf282375ef29bd31df651a717846c
alex randolph <alexgeorgerandolph@gmail.com>
2022-04-04 10:15:29 -0600

    Test: add test for item show page having all item attributes shown as well as project it belongs to

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Items show page' do
+
+  describe 'As a visitor' do
+
+    it 'I see the items name and cost, and name of project it belongs to' do
+
+      fence = Project.create!(name: 'New Fence Project', manager: 'Skeeter')
+      hammer = fence.items.create!(name: 'Big Ole Hammer', cost: "$55.00")
+
+      visit "/items/#{hammer.id}"
+      save_and_open_page
+      expect(page).to have_content("Name: #{hammer.name}")
+      expect(page).to have_content("Cost: #{hammer.cost}")
+      expect(page).to have_content("Project: #{fence.name}")
+
+
+
+    end
+
+
+
+
+
+  end
+
+
+
+
+
+
+
+end
