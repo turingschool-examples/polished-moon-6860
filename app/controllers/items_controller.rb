7a2efb126f9c54f8bec95e76f99865140b27c9d9
Sherman Au <shermau13@gmail.com>
2022-04-04 12:35:45 -0600

    refactor: remove strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,4 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
-  private
-
-  def item_params
-    params.permit(:id, :name, :cost)
-  end
 end
--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -2,10 +2,4 @@ class ManufacturersController < ApplicationController
   def index
     @manufacturers = Manufacturer.all
   end
-
-  private
-
-  def manufacturer_params
-    params.permit(:id, :name, :location)
-  end
 end
