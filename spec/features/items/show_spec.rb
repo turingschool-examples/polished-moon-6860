82c012d1d6d5ebe7c634fb36a4368caf9cff8c94
Sherman Au <shermau13@gmail.com>
2022-04-04 13:01:36 -0600

    test:#manufacturer_count on item#show path

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'item#show', type: :feature do
-  it "shows item's name, cost, and project associated with it" do
-    project_4 = Project.create(name: 'Project 4', manager: 'Tootoo')
-    item_3 = project_4.items.create!(name: 'Stick', cost: 22)
+  before do
+    @project_4 = Project.create(name: 'Project 4', manager: 'Tootoo')
+    @item_3 = @project_4.items.create!(name: 'Stick', cost: 22)
 
-    project_2 = Project.create(name: 'Project 2', manager: 'Bambi')
-    item_7 = project_2.items.create!(name: 'Ball', cost: 12)
+    @project_2 = Project.create(name: 'Project 2', manager: 'Bambi')
+    @item_7 = @project_2.items.create!(name: 'Ball', cost: 12)
 
     visit "/items/#{item_3.id}"
+  end
 
+  it "shows item's name, cost, and project associated with it" do
     expect(page).to have_content(item_3.name)
     expect(page).to have_content(item_3.cost)
 
