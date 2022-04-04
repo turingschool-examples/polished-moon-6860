3ebb39df04870876c6c62e3ecfccadbe13c0c114
Sherman Au <shermau13@gmail.com>
2022-04-04 13:04:30 -0600

    refactor:change item_3 to @item_3

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -2,19 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'item#show', type: :feature do
   before do
+    @manufacturer_3 = Manufacturer.create!(name: 'Manufacturer 3', location: 'Somewhere')
+    @manufacturer_6 = Manufacturer.create!(name: 'Manufacturer 6', location: 'Over There')
+    @manufacturer_1 = Manufacturer.create!(name: 'Manufacturer 1', location: 'Not Here')
+
     @project_4 = Project.create(name: 'Project 4', manager: 'Tootoo')
     @item_3 = @project_4.items.create!(name: 'Stick', cost: 22)
+    @item_6 = @project_4.items.create!(name: 'Wrench', cost: 35)
 
     @project_2 = Project.create(name: 'Project 2', manager: 'Bambi')
-    @item_7 = @project_2.items.create!(name: 'Ball', cost: 12)
+    @item_16 = @project_2.items.create!(name: 'Ball', cost: 12)
+    @item_7 = @project_2.items.create!(name: 'Pipe', cost: 54)
 
-    visit "/items/#{item_3.id}"
+    ManufacturerItem.create!(item_id: @item_3.id, manufacturer_id: @manufacturer_6.id)
+    ManufacturerItem.create!(item_id: @item_16.id, manufacturer_id: @manufacturer_3.id)
+    ManufacturerItem.create!(item_id: @item_16.id, manufacturer_id: @manufacturer_1.id)
+    ManufacturerItem.create!(item_id: @item_7.id, manufacturer_id: @manufacturer_6.id)
+    ManufacturerItem.create!(item_id: @item_6.id, manufacturer_id: @manufacturer_1.id)
+    ManufacturerItem.create!(item_id: @item_6.id, manufacturer_id: @manufacturer_3.id)
+
+    visit "/items/#{@item_3.id}"
   end
 
   it "shows item's name, cost, and project associated with it" do
-    expect(page).to have_content(item_3.name)
-    expect(page).to have_content(item_3.cost)
+    expect(page).to have_content(@item_3.name)
+    expect(page).to have_content(@item_3.cost)
 
-    expect(page).not_to have_content(item_7.name)
+    expect(page).not_to have_content(@item_7.name)
+  end
+
+  it 'shows count of manufacturers for specific item' do
+    save_and_open_page
+    expect(page).to have_content('Manufacturers: 1')
   end
 end
