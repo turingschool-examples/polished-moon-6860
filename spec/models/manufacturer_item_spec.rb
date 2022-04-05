af10f631a146df592559ebd81a5dcc32941175ae
Chris Kjolhede <ckjolhede@gmail.com>
2022-04-05 10:01:03 -0600

    feature: item show page show count of manufacturers

--- spec/models/manufacturer_item_spec.rb
+++ spec/models/manufacturer_item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ManufacturerItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'relationships' do
+    it { should belong_to(:manufacturer)}
+    it { should belong_to(:item)}
+  end
+
 end
