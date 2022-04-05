c8823e7d1827fb93ae79afe0124f3514680257fe
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-05 08:48:01 -0600

    test: model calculates average cost of items

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,12 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Project, type: :model do
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :manager }
   end
 
-  describe 'relationships' do
+  describe "relationships" do
     it { should have_many :items }
+  end
+
+  it "calculates average cost per item to two decimal places" do
+    wood_floor = Project.create!(name: "floor repair", manager: "Gary")
+    sander = wood_floor.items.create!(name: "sander", cost: 412.16)
+    varnish = wood_floor.items.create!(name: "varnish", cost: 102.45)
+
+    expect(wood_floor.average_item_cost).to eq(257.00)
   end
 end
