b367ad4d1cf9e24fa03f5693537e0073f2d87b9f
Alex Psitos <apsitos08@gmail.com>
2022-04-05 18:02:51 -0400

    Write test for user story 1

---
+++ spec/features/item/show_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'the item show page' do
+  it 'I see that items name and cost' do
+    project_1 = Project.create!(name: "Downtown Library", manager: "Steve")
+    item_1 = project_1.items.create!(name: "steel bars", cost: 14000)
+    visit "/items/#{item_1.id}"
+    expect(page).to have_content("#{item_1.name}")
+    expect(page).to have_content("#{item_1.cost}")
+  end
+end
