fedc8af60ed190da55c33d9816be233d2ebd839d
Will Medders <wmedders@gmail.com>
2022-04-05 14:36:48 -0400

    Feat: items show page displays item project name

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -12,4 +12,13 @@ RSpec.describe 'item show page' do
     expect(page).to have_content(project.name)
     expect(page).to have_no_content(item_2.name)
   end
+
+  it 'displays item project' do
+    project = Project.create!(name: "Pencil Project", manager: "Billy")
+    item = project.items.create!(name: "Pencil", cost: "50")
+    item_2 = project.items.create!(name: "Eraser", cost: "25")
+
+    visit "/items/#{item.id}"
+    expect(page).to have_content(project.name)
+  end
 end
