c1358be19780a093fed84f678537ecd725191c5f
Will Medders <wmedders@gmail.com>
2022-04-05 15:16:35 -0400

    Test: item model #manufacturer_count

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Item, type: :model do
     it { should have_many :manufacturer_items }
     it { should have_many(:manufacturers).through(:manufacturer_items) }
   end
+
+  describe 'instance methods' do
+    it '#manufacturer_count' do
+      project = Project.create!(name: "Pencil Project", manager: "Billy")
+      item = project.items.create!(name: "Pencil", cost: "50")
+      pencil_works = Manufacturer.create!(name: "Pencil Works", location: "China")
+      world_of_erasers = Manufacturer.create!(name: "World of Erasers", location: "Africa")
+      ManufacturerItem.create!(manufacturer_id: pencil_works.id, item_id: item.id)
+      ManufacturerItem.create!(manufacturer_id: world_of_erasers.id, item_id: item.id)
+      expect(item.manufacturer_count).to eq("2")
+    end
+  end
 end
