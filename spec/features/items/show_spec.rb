2c6eef90559c8b65891fa6c9df63ba99091e169b
Drew Proebstel <d.proebstel@gmail.com>
2022-04-05 15:23:24 -0700

    story 1

---
+++ spec/features/items/show_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'the application show' do
+  it "shows the application and all it's attributes" do
+    project = Project.create!(name: "Lazer Makers", manager: "Mike Dao")
+    item = project.items.create!(name: "Big Ass Lazer", cost: "12345")
+    visit "/items/#{item.id}"
+    save_and_open_page
+    expect(page).to have_content("Big Ass Lazer")
+    expect(page).to have_content("12345")
+    expect(page).to have_content("Lazer Makers")
+
+  end
+end
