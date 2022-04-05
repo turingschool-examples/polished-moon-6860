430a65423cfe48db097123992f03db86ec1510b7
Susan Boyd <sueboyd922@gmail.com>
2022-04-05 09:20:12 -0400

    Write test for extension story 1

---
+++ spec/features/projects/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'project show page' do
+  before (:each) do
+    @project1 = Project.create!(name: "New House", manager: "Joe Smith")
+    @project2 = Project.create!(name: "Ballpark", manager: "Fran Drescher")
+
+    @item1 = @project1.items.create!(name: "Chair", cost: 34)
+    @item2 = @project1.items.create!(name: "Bed frame", cost: 340)
+    @item3 = @project1.items.create!(name: "Blackout curtains", cost: 65)
+    @item4 = @project2.items.create!(name: "Field paint", cost: 50)
+    @item5 = @project2.items.create!(name: "Chain fence", cost: 480)
+    @item6 = @project2.items.create!(name: "Bleachers", cost: 750)
+  end
+
+  it 'shows the average cost per item needed for the project' do
+    visit "/projects/#{@project1.id}"
+    expect(page).to have_content(@project1.name)
+    expect(page).to have_content(@project1.avg_item_price)
+    expect(page).not_to have_content(@item2.name)
+
+    visit "/projects/#{@project2.id}"
+    expect(page).to have_content(@project2.name)
+    expect(page).to have_content(@project2.avg_item_price)
+    expect(page).not_to have_content(@item6.name)
+  end
+end
