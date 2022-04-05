9f4bd7cde53d9839f74c692da15a009912286181
Will Medders <wmedders@gmail.com>
2022-04-05 14:59:06 -0400

    Test: man... index lists all the names of items they belong to underneath each manufacturer

--- spec/features/manufacturers/index_spec.rb
+++ spec/features/manufacturers/index_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe 'manufacturers index page' do
     expect(page).to have_content(pencil_works.name)
     expect(page).to have_content(world_of_erasers.name)
   end
+
+  it 'lists all the names of items they belong to underneath each manufacturer' do
+    project = Project.create!(name: "Pencil Project", manager: "Billy")
+    item = project.items.create!(name: "Pencil", cost: "50")
+    item_2 = project.items.create!(name: "Eraser", cost: "25")
+    pencil_works = Manufacturer.create!(name: "Pencil Works", location: "China")
+    world_of_erasers = Manufacturer.create!(name: "World of Erasers", location: "Africa")
+    ManufacturerItem.create!(manufacturer_id: pencil_works.id, item_id: item.id)
+    ManufacturerItem.create!(manufacturer_id: world_of_erasers.id, item_id: item_2.id)
+
+    visit "/manufacturers"
+    expect(page).to have_content(item.name)
+    expect(page).to have_content(item_2.name)
+  end
 end
