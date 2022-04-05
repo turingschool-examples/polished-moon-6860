135e944faaee3d2b0b1317ec1d8fe59bb6cd7f1d
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-05 08:06:33 -0600

    test: model finds number of manufacturers associated with item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,14 +1,30 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  describe 'validations' do
+  describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :cost }
   end
 
-  describe 'relationships' do
+  describe "relationships" do
     it { should belong_to :project }
     it { should have_many :manufacturer_items }
     it { should have_many(:manufacturers).through(:manufacturer_items) }
+  end
+
+  it "counts manufacturers associated with item" do
+    wood_floor = Project.create!(name: "floor repair", manager: "Gary")
+    sander = wood_floor.items.create!(name: "sander", cost: 4)
+    varnish = wood_floor.items.create!(name: "varnish", cost: 10)
+
+    remodel = Manufacturer.create!(name: "Remodel", location: "Golden, CO")
+    diy = Manufacturer.create!(name: "DIY Tools", location: "Arvada, CO")
+
+    sander.manufacturers << remodel
+    varnish.manufacturers << remodel
+    varnish.manufacturers << diy
+
+    expect(sander.manufacturer_count).to eq(1)
+    expect(varnish.manufacturer_count).to eq(2)
   end
 end
