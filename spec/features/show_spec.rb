d046e864b294509362ea0d808143e2141d725f55
Luke Pascale <lpluke5@gmail.com>
2022-04-05 15:58:06 -0500

    FeatTest: Item show page displays item name, cost and project name

---
+++ spec/features/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'the Item show page' do
+  before(:each) do
+    @project = Project.create!(name: 'Alpha Omega', manager: 'Luke P')
+    @item = @project.items.create!(name: 'Adamantium Tube', cost: 743)
+  end
+
+  it 'shows an items name and cost' do
+    visit '/items/:id'
+
+    expect(page).to have_content(@item.name)
+    expect(page).to have_content(@item.cost)
+  end
+
+  it 'shows what project the item belongs to' do
+    visit '/items/:id'
+
+    expect(page).to have_content(@project.name)
+  end
+end
