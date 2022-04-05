d27c0f91d4c4880f7dc30e44a6fcb597bcd9af3d
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-05 17:22:19 -0600

    User Story 3, complete

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Item Show Page' do
+RSpec.describe "Item Show Page" do
   it "displays the items name and cost along with name of project it belongs to" do
     project = Project.create!(name: "New Tech", manager: "Mike Dao")
     item = Item.create!(name: "Laptop", cost: 250, project_id: project.id)
@@ -10,5 +10,17 @@ RSpec.describe 'Item Show Page' do
     expect(page).to have_content(item.name)
     expect(page).to have_content(item.cost)
     expect(page).to have_content(item.project.name)
+  end
+
+  it "displays the count of number of manufacturers for this item" do
+    project1 = Project.create!(name: "New Tech", manager: "Mike Dao")
+    item1 = Item.create!(name: "Laptop", cost: 250, project_id: project1.id)
+    manufacturer1 = item1.manufacturers.create!(name: "Widget Maker", location: "Phoenix")
+    manufacturer2 = item1.manufacturers.create!(name: "Gear Heads", location: "Sacramento")
+    manufacturer3 = item1.manufacturers.create!(name: "Weavers", location: "Chicago")
+
+    visit "/items/#{item1.id}"
+
+    expect(page).to have_content(item1.manufacturers.count)
   end
 end
